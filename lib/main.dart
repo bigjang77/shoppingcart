111bd13681f1d4bcfb8432bd6d2ac8fd16af5b55
bigjang77 <dhex2004@gmail.com>
2022-10-06 09:51:45 +0900

    쇼핑카드 헤더,디테일,메인하는중

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:shoppingcart/components/shoppingcart_detail.dart';
+import 'package:shoppingcart/components/shoppingcart_header.dart';
+import 'package:shoppingcart/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme(),
+      home: ShoppingCartPage(),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildShoppingCartAppbar(),
+      body: Column(
+        children: [
+          ShoppingCartHeader(),
+          ShoppingCartDetail(),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildShoppingCartAppbar() {
+    return AppBar(
+      title: Text("ShoppingCart"),
+    );
+  }
+}
